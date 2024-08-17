8373c9558bb39d5bfaac1cd3ef271ce957519cc8
kdntiaoao <iadokonat0106@gmail.com>
2024-08-18 00:09:09 +0900

    chore: update default stroke width to 4

--- src/constants/default-values.ts
+++ src/constants/default-values.ts
@@ -1,7 +1,7 @@
 export const DEFAULT_VALUES = {
   fontWeight: 700,
   textColor: "#ffffff",
-  strokeWidth: 6,
+  strokeWidth: 4,
   directionCount: 40,
   strokeColor: "#000000",
   sampleText: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel ab incidunt aliquid atque sit. Natus reiciendis eum in praesentium cumque velit eos expedita, quod odio, illum quaerat, consectetur rerum quos?
