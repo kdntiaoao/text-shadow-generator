7f29143c25219d62b5cce7759be006bbbcfb3fca
kdntiaoao <kodai.t@chot-inc.com>
2025-01-03 14:32:01 +0900

    chore: configure prettier and eslint settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
-import path from "path";
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
+    alias: { '@': path.resolve(__dirname, './src') },
   },
-});
+})
