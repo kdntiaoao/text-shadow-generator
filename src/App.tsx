a890d6a05061eac9b050cf07d7c3b4d3fbf772d8
kdntiaoao <kodai.t@chot-inc.com>
2025-04-27 13:34:43 +0900

    Create Vite App

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,34 @@
-import Sidebar from '@/components/sidebar'
-import StrokeText from '@/components/stroke-text'
-import { useAtomValue } from 'jotai'
-import { valuesAtom } from '@/states/values'
-import { textShadowAtom } from '@/states/text-shadow'
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
-  const values = useAtomValue(valuesAtom)
-  const textShadow = useAtomValue(textShadowAtom)
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="md:flex">
-      <div className="min-w-64 overflow-hidden md:sticky md:bottom-0 md:left-0 md:top-0 md:max-h-screen md:w-1/4 md:max-w-96 md:flex-shrink-0">
-        <Sidebar />
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-
-      <div
-        className="grid flex-1 gap-10 p-8 text-5xl font-bold"
-        style={{ fontWeight: values.fontWeight }}
-      >
-        <div>
-          <StrokeText textColor={values.textColor} textShadow={textShadow}>
-            {values.sampleText}
-          </StrokeText>
-        </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
       </div>
-    </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 
--- src/utils/validate-font-weight.ts
+++
@@ -1,5 +0,0 @@
-import { FontWeight } from '@/types/values'
-
-export const validateFontWeight = (value: number): value is FontWeight => {
-  return [100, 200, 300, 400, 500, 600, 700, 800, 900].includes(value)
-}
