c844ad6d4fd4d5cc2d81987933c14862b0929ed4
kdntiaoao <kodai.t@chot-inc.com>
2025-05-12 08:05:08 +0900

    fix: refactoring

--- src/utils/generate-text-shadow.ts
+++ src/utils/generate-text-shadow.ts
@@ -1,17 +1,17 @@
 type Params = {
   /** 縁取りの幅 */
   baseWidth: number;
-  /** text-shadowを生成する方向の数 (値を小さくすると縁取りが粗くなる) */
+  /** text-shadowを生成する方向の数 */
   directionCount?: number;
   /** 縁取りの色 */
   color: string;
   /** ぼかし */
   blur?: number;
-  /** 幅の間隔 (細い文字で縁取りの幅が広いと空白が発生することがあるので、その対策) */
+  /** 幅の間隔 */
   radiusStep?: number;
-  /** 一番外側の text-shadow の値の小数点の桁数 (設定するとジャギー軽減になる) */
+  /** 一番外側の text-shadow の値の小数点の桁数 */
   digits?: number;
-  /** 影のオフセット (影をつけたいときに設定する、縁取りのみの場合は 0 ) */
+  /** 影のオフセット */
   shadowOffset?: number;
   /** 影の色 */
   shadowColor?: string;
@@ -33,6 +33,11 @@ export function generateTextShadow({
   shadowColor = color,
   addShadows,
 }: Params): string {
+  if (!baseWidth || !directionCount || blur < 0 || !radiusStep || digits < 0) {
+    console.error("Invalid parameters value");
+    return "";
+  }
+
   const blurValue = `${blur}px`;
   const shadows: Set<string> = new Set();
   const shadowOffsets: Set<string> = new Set();
@@ -46,22 +51,14 @@ export function generateTextShadow({
       angle < 2 * Math.PI;
       angle += (2 * Math.PI) / directionCount
     ) {
-      const x =
-        radius === currentMaxRadius
-          ? Math.round(radius * Math.cos(angle) * 10 ** digits) / 10 ** digits
-          : Math.round(radius * Math.cos(angle));
-      const y =
-        radius === currentMaxRadius
-          ? Math.round(radius * Math.sin(angle) * 10 ** digits) / 10 ** digits
-          : Math.round(radius * Math.sin(angle));
+      const x = Number((radius * Math.cos(angle)).toFixed(digits));
+      const y = Number((radius * Math.sin(angle)).toFixed(digits));
       const valueX = `${x}px`;
       const valueY = `${y}px`;
       shadows.add(`${valueX} ${valueY} ${blurValue} ${color}`);
       if (shadowOffset) {
-        const shadowX =
-          Math.round((x + shadowOffset) * 10 ** digits) / 10 ** digits;
-        const shadowY =
-          Math.round((y + shadowOffset) * 10 ** digits) / 10 ** digits;
+        const shadowX = Number((x + shadowOffset).toFixed(digits));
+        const shadowY = Number((y + shadowOffset).toFixed(digits));
         const valueShadowX = `${shadowX}px`;
         const valueShadowY = `${shadowY}px`;
         shadowOffsets.add(
@@ -81,26 +78,16 @@ export function generateTextShadow({
           angle < 2 * Math.PI;
           angle += (2 * Math.PI) / directionCount
         ) {
-          const x =
-            radius === currentMaxRadius
-              ? Math.round(radius * Math.cos(angle) * 10 ** digits) /
-                10 ** digits
-              : Math.round(radius * Math.cos(angle));
-          const y =
-            radius === currentMaxRadius
-              ? Math.round(radius * Math.sin(angle) * 10 ** digits) /
-                10 ** digits
-              : Math.round(radius * Math.sin(angle));
-          const valueX = x === 0 ? "0" : `${x}px`;
-          const valueY = y === 0 ? "0" : `${y}px`;
+          const x = Number((radius * Math.cos(angle)).toFixed(digits));
+          const y = Number((radius * Math.sin(angle)).toFixed(digits));
+          const valueX = `${x}px`;
+          const valueY = `${y}px`;
           shadows.add(`${valueX} ${valueY} ${blurValue} ${addShadow.color}`);
           if (shadowOffset) {
-            const shadowX =
-              Math.round((x + shadowOffset) * 10 ** digits) / 10 ** digits;
-            const shadowY =
-              Math.round((y + shadowOffset) * 10 ** digits) / 10 ** digits;
-            const valueShadowX = shadowX === 0 ? "0" : `${shadowX}px`;
-            const valueShadowY = shadowY === 0 ? "0" : `${shadowY}px`;
+            const shadowX = Number((x + shadowOffset).toFixed(digits));
+            const shadowY = Number((y + shadowOffset).toFixed(digits));
+            const valueShadowX = `${shadowX}px`;
+            const valueShadowY = `${shadowY}px`;
             shadowOffsets.add(
               `${valueShadowX} ${valueShadowY} ${blurValue} ${shadowColor}`,
             );
